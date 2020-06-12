414d74811b99aee17d9892e664f0461e807eeb9d
Francisco Mondaca <f.mondaca@uni-koeln.de>
2020-06-12 11:32:21 +0200

    index_name as var

--- eisp/extract_and_index.py
+++ eisp/extract_and_index.py
@@ -14,10 +14,17 @@ def get_pdf_content(file):
     text = file_data['content']
     return text
 
-def create_index(path_to_elastic_mapping):
+def create_index(path_to_elastic_mapping, index_name):
     idxs = connections.get_connection().indices
-    logger().debug('Creating index %s', 'eisp')
-    idxs.create(index='eisp', body=load_elastic_mapping(path_to_elastic_mapping))
+    logger().info('Creating index %s', index_name)
+    idxs.create(index=index_name, body=load_elastic_mapping(path_to_elastic_mapping))
+
+
+def delete_index(index_name):
+    idxs = connections.get_connection().indices
+    logger().info('Dropping index %s', index_name)
+    idxs.delete(ignore = 404, index = index_name)
+
 
 def index_pdfs(index_name, path_to_pdfs):
     pdfs = get_pdf_files(path_to_pdfs)
