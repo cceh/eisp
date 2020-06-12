f58588eb7949c38a9db39e8484854c1743972fd8
Francisco Mondaca <f.mondaca@uni-koeln.de>
2020-06-12 11:33:30 +0200

    - reading eisp.ini properly - deleting index before creating it

--- eisp/eisp.py
+++ eisp/eisp.py
@@ -2,9 +2,10 @@ from sys import argv
 from os import getpid
 from elasticsearch import helpers
 from elasticsearch_dsl import connections
+from importlib import import_module as mod
 from logging import getLogger, basicConfig
 from eisp.utils import logger, dotdict, instance, defaultconfig
-from eisp.extract_and_index import index_pdfs, create_index
+from eisp.extract_and_index import index_pdfs, create_index, delete_index
 from configparser import ConfigParser
 
 def main() -> None: eisp().main()
@@ -31,10 +32,18 @@ class eisp():
             instance.config = ConfigParser()
             instance.config.read_dict(defaultconfig())
             logger().info('Started eisp with pid %s', getpid())
+
+            for i in [i for i in argv if i.startswith('--')]:
+                try:
+                    mod('eisp.param.{}'.format(i[2:])).__dict__[i[2:]](argv)
+                except:
+                    exit('Invalid parameter or argument to {}'.format(i[2:]))
+
             conf = dotdict(instance.config['data'])
             connections.create_connection(hosts=[conf.host])
-            create_index(conf.elastic_mapping)
-            helpers.bulk(connections.get_connection(), index_pdfs('eisp', conf.root))
+            delete_index(conf.index_name)
+            create_index(conf.elastic_mapping, conf.index_name)
+            helpers.bulk(connections.get_connection(), index_pdfs(conf.index_name, conf.root))
 
         except KeyboardInterrupt:
             print('\N{bomb}')
