52007023a5503f5d1d1110db9551d1ea37be76a2
Francisco Mondaca <f.mondaca@uni-koeln.de>
2020-07-17 09:48:58 +0200

    - pymupdf removed - general exception handling - fixed: indexing only pages with content

--- eisp/extract_and_index.py
+++ eisp/extract_and_index.py
@@ -2,25 +2,21 @@ import glob
 import os
 import gc
 from elasticsearch_dsl import connections
-import fitz
 from eisp.utils import logger, load_elastic_mapping
 import PyPDF2
 
-try:
-    TOOLS = fitz.TOOLS
-except:
-    TOOLS = fitz.Tools()
-gc.set_debug(gc.DEBUG_UNCOLLECTABLE)
 
-
-def extractPYPDF(filename):
+def extract_pdf(filename):
     logger().info('Reading: %s', filename)
     with open(filename, mode='rb') as f:
-        reader = PyPDF2.PdfFileReader(f)
-        number_of_pages = reader.getNumPages()
-        for page_number in range(number_of_pages):
-            page = reader.getPage(page_number)
-            yield page.extractText()
+        try:
+            reader = PyPDF2.PdfFileReader(f)
+            number_of_pages = reader.getNumPages()
+            for page_number in range(number_of_pages):
+                page = reader.getPage(page_number)
+                yield page.extractText()
+        except:
+            logger().error('Could not read: %s', filename)
 
 
 def get_pdf_files(path_to_dir):
@@ -40,43 +36,22 @@ def delete_index(index_name):
     idxs.delete(ignore=404, index=index_name)
 
 
-def extract_pdf(filename):
-    logger().info('Reading: %s', filename)
-    try:
-        doc = fitz.Document(filename)
-        number_of_pages = doc.pageCount
-        for page_number in range(number_of_pages):
-            page = doc.loadPage(page_number)
-            page_content = page.getText('text')
-            if page_content:
-                yield page_content
-
-        # logger().info('mupdf size A: %s', TOOLS.store_size)
-        doc.close()
-        TOOLS.store_shrink(100)  # reset MuPDF global context
-        gc.collect()
-        # logger().info('mupdf size B: %s', TOOLS.store_size)
-
-    except RuntimeError:
-        logger().error('Could not open: %s', filename)
-
-
 def index_pdfs(index_name, path_to_pdfs):
     pdfs = get_pdf_files(path_to_pdfs)
     for i, e in enumerate(pdfs):
         doc_name = e.replace('/var/lib/eisp/', '')
-        # pdf_content = extract_pdf(e)
-        pdf_content = extractPYPDF(e)
+        pdf_content = extract_pdf(e)
         for j, c in enumerate(pdf_content):
             j += 1
-            yield {
-                "_index": index_name,
-                "page_id": doc_name + '_page_' + str(j),
-                "page_content": c,
-                "pdf_name": doc_name
+            if c:
+                yield {
+                    "_index": index_name,
+                    "page_id": doc_name + '_page_' + str(j),
+                    "page_content": c,
+                    "pdf_name": doc_name
 
-            }
-            del c
-            gc.collect()
+                }
+                del c
+                gc.collect()
 
         del pdf_content
